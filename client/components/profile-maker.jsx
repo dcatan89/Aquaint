c1a6e468986cdda2740e3b3298534ddb1b4ee77a
dcatan89 <danieljcatan@gmail.com>
2022-04-15 18:54:34 +0000

    profile front-end completed

--- client/components/profile-maker.jsx
+++ client/components/profile-maker.jsx
@@ -28,6 +28,7 @@ export default class MakeProfile extends React.Component {
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.renderButton = this.renderButton.bind(this);
   }
 
   handleChange(event) {
@@ -36,31 +37,48 @@ export default class MakeProfile extends React.Component {
 
   handleSubmit(e) {
     const form = document.querySelector('form');
-    const { index } = this.state;
-    index > 4
-      ? this.setState({ index: 0 })
-      : this.setState({ index: this.state.index + 1 });
+    this.setState({ index: this.state.index + 1 });
     form.reset();
     e.preventDefault();
   }
 
-  render() {
+  renderButton() {
     const { index } = this.state;
+    if (index > 5) {
+      return (
+          <div className="container">
+            <div className="row half-height justify-content-center align-items-center">
+              <h1 className="text-center text-light col-sm-6 font-lg" >Profile Created</h1>
+            </div>
+            <a href="#" className="row">
+              <div className="col col-lg-12 text-center">
+                <Button classes={'btn btn-outline-light  col-6 col-sm-6 rounded-pill'} text={'Proceed'} />
+              </div>
+            </a>
+          </div>
+      );
+    }
     return (
-      <div className="container">
-        <form onSubmit={this.handleSubmit}>
-          <div className="row">
-            <h1 className="text-center col-sm-6" >{topicsArr[index]}</h1>
-            <div className='col-sm-6'>
-              <input onChange={this.handleChange} required className="form-control form-control" type="text" placeholder={inputsArr[index]} aria-label=".form-control" />
-            </div>
+      <form className="container"onSubmit={this.handleSubmit}>
+        <div className="row half-height align-items-center">
+          <h1 className="text-center text-light col-sm-6 font-lg" >{topicsArr[index]}</h1>
+          <div className='col-sm-6'>
+            <input onChange={this.handleChange} required className="form-control form-control" type="text" placeholder={inputsArr[index]} aria-label=".form-control" />
           </div>
-          <div className="row col justify-content-center align-items-center text-center">
-            <div>
-              <Button type={'submit'} click={this.handleSubmit} classes={'btn btn-outline-light bgc-gradient col-6 col-sm-6 rounded-pill'} text={'Continue'} />
-            </div>
+        </div>
+        <div className="row justify-content-center align-items-center half-height">
+          <div className="col text-center">
+            <Button type={'submit'} click={this.handleSubmit} classes={'btn btn-outline-light col-6 col-sm-6 rounded-pill'} text={'Continue'} />
           </div>
-        </form>
+        </div>
+      </form>
+    );
+  }
+
+  render() {
+    return (
+      <div className="bgc-gradient">
+        {this.renderButton()}
       </div>
     );
   }
