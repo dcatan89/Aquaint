09736fb9373591407f53dc5e35c7439d34ab1e02
dcatan89 <danieljcatan@gmail.com>
2022-04-29 21:49:32 +0000

    default values corrected in update form

--- client/components/form.jsx
+++ client/components/form.jsx
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
 
 export default function UserForm(props) {
-  const userData = {
-    fullName: `${props.fullName}`,
-    occupation: `${props.occupation}`
+  const [value] = useState({ name: props.fullName, occupation: props.occupation, fact: props.fact, birthday: props.birthday, sex: props.sex, profileId: props.profileId });
+  const handleSubmit = e => {
+    e.preventDefault();
+    location.hash = `matchProfile?profileId=${value.profileId}`;
   };
-  const [value, onSubmit] = useState({ name: userData.fullName, occupation: userData.occupation });
-
   return (
-    <form onSubmit={onSubmit} >
+    <form onSubmit={handleSubmit} >
       <input defaultValue={value.name} name='fullName' className="form-control form-control-lg" type="text" placeholder=".form-control-lg" aria-label=".form-control-lg example" />
       <input defaultValue={value.occupation} name='occupation' className="form-control form-control-lg" type="text" placeholder=".form-control-lg" aria-label=".form-control-lg example" />
+      <input defaultValue={value.fact} name='occupation' className="form-control form-control-lg" type="text" placeholder=".form-control-lg" aria-label=".form-control-lg example" />
+      <input defaultValue={value.birthday} name='occupation' className="form-control form-control-lg" type="text" placeholder=".form-control-lg" aria-label=".form-control-lg example" />
+      <input defaultValue={value.sex} name='occupation' className="form-control form-control-lg" type="text" placeholder=".form-control-lg" aria-label=".form-control-lg example" />
+      <button>Edit Profile</button>
     </form>
   );
 }
--- client/pages/edit-profile.jsx
+++ client/pages/edit-profile.jsx
@@ -12,12 +12,22 @@ export default function EditProfile(props) {
       });
   }, []);
 
+  const editProfile = userData => {
+    fetch('/api/matchProfiles/apce', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userData)
+    })
+      .then(response => response.json());
+  };
   return (
     <div className="bgc-gradient-3 vh100">
       <Navigation />
-      <div className="container">
-        {user ? <Form fullName={user.fullName} occupation={user.occupation} /> : null}
-        {user && <img width={'1000px'} height={'1000px'} src={user.image}></img>}
+      <div className="container vh100">
+        {user ? <Form onSubmit={editProfile} fullName={user.fullName} occupation={user.occupation} birthday={user.birthday} sex={user.sex} fact={user.fact} profileId={user.profileId} /> : null}
+        {user && <img width={'1000px'} height={'1000px'} src={user.image}/>}
       </div>
     </div>
   );
